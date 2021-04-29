910e3fe5c994a42981b707c9ef648a89b5ab998a
roger <roger@siliconrhino.io>
2021-04-29 14:31:08 +0100

    testing applying primary color

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
-import React from "react"
-import Helmet from 'react-helmet';
-import { graphql } from 'gatsby'
-import Layout from "../components/layout"
-import PostLink from "../components/post-link"
-import HeroHeader from "../components/heroHeader"
+import React from "react";
+import Helmet from "react-helmet";
+import { graphql } from "gatsby";
+import Layout from "../components/layout";
+import PostLink from "../components/post-link";
+import HeroHeader from "../components/heroHeader";
 
 const IndexPage = ({
   data: {
@@ -11,33 +11,33 @@ const IndexPage = ({
     allMarkdownRemark: { edges },
   },
 }) => {
-
   const Posts = edges
-    .filter(edge => !!edge.node.frontmatter.date) // You can filter your posts based on some criteria
-    .map(edge => <PostLink key={edge.node.id} post={edge.node} />)
+    .filter((edge) => !!edge.node.frontmatter.date) // You can filter your posts based on some criteria
+    .map((edge) => <PostLink key={edge.node.id} post={edge.node} />);
 
   return (
     <Layout>
       <Helmet>
-        <title>{site.siteMetadata.title}</title>
+        <title style={{ color: site.siteMetadata.theme.primaryColor }}>
+          {site.siteMetadata.title}
+        </title>
         <meta name="description" content={site.siteMetadata.description} />
       </Helmet>
-      <HeroHeader/>
+      <HeroHeader />
       <h2>Blog Posts &darr;</h2>
-      <div className="grids">
-        {Posts}
-      </div>
+      <div className="grids">{Posts}</div>
     </Layout>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
 export const pageQuery = graphql`
   query indexPageQuery {
     site {
       siteMetadata {
         title
         description
+        theme
       }
     }
     allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
@@ -55,4 +55,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
