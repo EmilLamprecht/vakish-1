f27f319aed76ff3b2bc42c97d365e3f09461c7d5
roger <roger@siliconrhino.io>
2021-06-17 14:29:32 +0100

    testing new form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -55,26 +55,22 @@ const ContactPage = ({ data: { site } }) => {
           <p>Let me help you kick start your next project &rarr;</p>
         </div>
         <div>
-          <form
-            className="form-container"
-            action="https://sendmail.w3layouts.com/SubmitContactForm"
-            method="post"
-          >
+          <form className="form-container" data-netlify="true" method="post">
             <div>
               <label htmlFor="w3lName">Name</label>
-              <input type="text" name="w3lName" id="w3lName" />
+              <input type="text" name="name" id="w3lName" />
             </div>
             <div>
               <label htmlFor="w3lSender">Email</label>
-              <input type="email" name="w3lSender" id="w3lSender" />
+              <input type="email" name="email" id="w3lSender" />
             </div>
             <div>
               <label htmlFor="w3lSubject">Subject</label>
-              <input type="text" name="w3lSubject" id="w3lSubject" />
+              <input type="text" name="subject" id="w3lSubject" />
             </div>
             <div>
               <label htmlFor="w3lMessage">Message</label>
-              <textarea name="w3lMessage" id="w3lMessage"></textarea>
+              <textarea name="message" id="w3lMessage"></textarea>
             </div>
             <div style={{ display: "flex", justifyContent: "flex-end" }}>
               <input
