5a8e4824e27f57a958b2d009773578a4ac0d5915
roger <roger@siliconrhino.io>
2024-03-01 15:58:03 +0100

    test

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -99,15 +99,18 @@ export default function Template({
           font-weight: bold;
         }
     `}</style>
-        
+
         <script>
+          {`
           var script = document.createElement('script'); script.id = 'fathom';
           script.dataset.site = 'PKHMMWAW';
           script.src = "https://cdn.usefathom.com/script.js";
           document.getElementsByTagName('head')[0].appendChild(script);
+          `}
         </script>
 
         <script>
+          {`
           (function (s, e, n, d, er) {
             s['Sender'] = er;
             s[er] = s[er] || function () {
@@ -120,7 +123,8 @@ export default function Template({
             m.parentNode.insertBefore(a, m)
             })(window, document, 'script', 'https://cdn.sender.net/accounts_resources/universal.js', 'sender');
             sender('b985bb7a815249')
-          </script>
+            `}
+        </script>
       </Helmet>
       <div className="blog-post-container">
         <article className="post">
