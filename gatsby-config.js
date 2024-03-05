65204d85394d9a59a8a0cb11023415d7a4f4ca9f
EmilLamprecht <EmilLamprecht@users.noreply.github.com>
2024-03-05 15:43:28 +0100

    add fathom and sender, remove google plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,25 +36,6 @@ module.exports = {
       },
     },
     {
-      resolve: "gatsby-plugin-google-tagmanager",
-      options: {
-        id: "GTM-WXRQM2GT",
-
-        // Include GTM in development.
-        //
-        // Defaults to false meaning GTM will only be loaded in production.
-        includeInDevelopment: true,
-      },
-    },
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        // The property ID; the tracking code won't be generated without it. replace with yours
-        trackingId: "UA-164743872-2",
-        head: true,
-      },
-    },
-    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `Delog GatbsyJS Starter`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -87,6 +87,31 @@ const IndexPage = ({
           font-weight: bold;
         }
     `}</style>
+        <script>
+          {`
+            var script = document.createElement('script'); script.id = 'fathom';
+            script.dataset.site = 'ZQAWFGYG';
+            script.src = "https://cdn.usefathom.com/script.js";
+            document.getElementsByTagName('head')[0].appendChild(script);
+          `}
+        </script>
+
+        <script>
+          {`
+              (function (s, e, n, d, er) {
+                s['Sender'] = er;
+                s[er] = s[er] || function () {
+                  (s[er].q = s[er].q || []).push(arguments)
+                }, s[er].l = 1 * new Date();
+                var a = e.createElement(n),
+                    m = e.getElementsByTagName(n)[0];
+                a.async = 1;
+                a.src = d;
+                m.parentNode.insertBefore(a, m)
+              })(window, document, 'script', 'https://cdn.sender.net/accounts_resources/universal.js', 'sender');
+              sender('b985bb7a815249')
+          `}
+        </script>
       </Helmet>
       <HeroHeader />
       <h2>Blog Posts &darr;</h2>
